6eb03fef2fd04aa7c73c137f54bd66055b9ce483
wenzzzel <wenzzzel@live.se>
2023-04-06 19:07:12 +0200

    feature: Added some sample logic for deleting cosmos documents

---
+++ Models/SampleModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace cosmosdb_cli.Models;
+
+public class SampleModel
+{
+    public string Id { get; set; }
+    public string PartitionKey { get; set; }
+    // Add more fields...
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using Azure.Identity;
+using cosmosdb_cli.Models;
 using Microsoft.Azure.Cosmos;
 
+/*********
+* SETUP *
+*********/
 // Set up authentication and authorization with Azure managed identity
 var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions());
 var options = new CosmosClientOptions
@@ -13,26 +17,79 @@ var options = new CosmosClientOptions
 };
 
 // Set up the Cosmos client
-var endpointUri = "https://emea-servicedata-logic-qa.documents.azure.com:443/";
+var endpointUri = "ENPOINT_URI";
 var client = new CosmosClient(endpointUri, credential, options);
 
 // Set up the container
 var databaseName = "DATABASE_NAME";
-var containerName = "CONTAINER_NAME";  
+var containerName = "CONTAINER_NAME";
 var database = client.GetDatabase(databaseName);
 var container = database.GetContainer(containerName);
 
-// Delete a record
-var id = "DOCUMENT_ID";
-var partitionKey = new PartitionKey("DOCUMENT_PARTITION_KEY");
-var result = await container.DeleteItemAsync<dynamic>(id, partitionKey);
 
-// Check the status of the operation
-if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+/************************
+ * DELETE SINGLE RECORD *
+ ************************/
+// TODO: control with parameters if below method is run or not
+// Delete a record
+var deleteResult = await DeleteItemAsync(
+    container,
+    "DRU2711~1400001542",
+    "YV1BZ8256C1135673");
+
+
+/*********************************************
+ * DELETE MANY RECORDS BASED ON QUERY RESULT *
+ *********************************************/
+// TODO: Control with parameters if below method is run or not
+// Fetch selection with sql query
+var queryResult = await GetItemsBySQLAsync<SampleModel>(
+    "SELECT * FROM c where 1 = 1",
+    client,
+    database,
+    container);
+
+var datas = new List<(string id, string partitionKey)>();
+
+foreach (var item in queryResult)
+    datas.Add((item.Id, item.PartitionKey));
+
+// Delete all fetched records
+await DeleteItemsAsync(
+    container,
+    datas);
+async Task<IEnumerable<T>> GetItemsBySQLAsync<T>(string sqlQuery, CosmosClient client, Database database, Container container)
 {
-    Console.WriteLine("Record deleted successfully");
+    var query = new QueryDefinition(sqlQuery);
+
+    var requestOptions = new QueryRequestOptions()
+    {
+        MaxItemCount = -1,
+        ResponseContinuationTokenLimitInKb = 3
+    };
+
+    FeedIterator<T> resultSet = client.GetContainer(database.Id, container.Id)
+        .GetItemQueryIterator<T>(query, requestOptions: requestOptions);
+
+    List<T> items = new List<T>();
+    while (resultSet.HasMoreResults)
+    {
+        items.AddRange(await resultSet.ReadNextAsync());
+    }
+    return items;
 }
-else
+
+async Task<ItemResponse<dynamic>> DeleteItemAsync(Container container, string id, string partitionKey)
 {
-    Console.WriteLine($"Failed to delete record. Status code: {result.StatusCode}");
+    var result = await container.DeleteItemAsync<dynamic>(id, new PartitionKey(partitionKey));
+    Console.WriteLine($"Deleted item with id: {id} and partitionKey {partitionKey}");
+    return result;
+}
+
+async Task DeleteItemsAsync(Container container, List<(string id, string partitionKey)> items)
+{
+    foreach (var (id, partitionKey) in items)
+        await DeleteItemAsync(container, id, partitionKey);
+
+    return;
 }
